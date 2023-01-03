d3c630d0d9e50af092a47ecf3af4406a9d52d686
Mikhail Turilov <turilovm@gmail.com>
2023-01-03 18:44:51 +0300

    add test search

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -1,8 +1,10 @@
 package tests;
 
 import com.codeborne.selenide.Condition;
+import dev.failsafe.internal.util.Assert;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static io.qameta.allure.Allure.step;
@@ -13,7 +15,8 @@ public class SearchTest extends TestBase {
     void searchTest (){
         MainPage mainPage = new MainPage(BASE_URL);
         step("Проверка поиска товара", () -> {
-
+            $("#searchInput").setValue("Pixel 7").pressEnter();
+            $(".snow-container_SnowContainer__container__hu0uqp").shouldHave(text("Pixel 7"));
         });
     }
 
